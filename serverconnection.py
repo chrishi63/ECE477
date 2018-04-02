232cf14dfe572f607b3189af7db9632ec51dfc3c
chrishi63 <shi207@purdue.edu>
2018-04-02 02:23:45 +0000

    working test server connection

--- serverconnection.py
+++ serverconnection.py
@@ -1,13 +1,29 @@
 import mysql.connector
 
-config = {
-    'user': '',
-    'password': '',
-    'host': '',
-    'database': '',
-    'raise_on_warnings': True,
-}
+cnx = mysql.connector.connect(user='ece477', password='SET56?ra', \
+                              host='mysql.stackcp.com',\
+                              database='cl57-users-xyq', port='50728')
+if cnx.is_connected():
+    print('Connected to MySQL database')
+cur = cnx.cursor()
 
-cnx = mysql.connector.connect(**config)
+query = ("SELECT * FROM users;")
 
+cur.execute(query)
+
+for (num, name, email, password, dummy, client, server) in cur:
+  print("{}, {}, {}, {}, {}, {}".format(num, name, email, password, client, server))
+
+#insertquery = ("INSERT INTO users "
+#               "(email, password, nameTag, client, server)"
+#"VALUES ('abc@purdue.edu', 'abcdef234','abc2491','test16','test29');")
+
+newquery = ("SELECT CURDATE();")
+#cur.execute(insertquery)
+cur.execute(newquery)
+for value in cur:
+    print(value[0])
+
+#cnx.commit()
+cur.close()
 cnx.close()
