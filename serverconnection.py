330fc92fca60a04c1af99c739786793fcde68c32
Christina Shi <shi207@purdue.edu>
2018-04-04 18:32:45 -0400

    updated credentials

--- serverconnection.py
+++ serverconnection.py
@@ -1,18 +1,36 @@
+
+# http://web.ics.purdue.edu/software/phpMyAdmin/
+
 import mysql.connector
 
-cnx = mysql.connector.connect(user='ece477', password='SET56?ra', \
-                              host='mysql.stackcp.com',\
-                              database='cl57-users-xyq', port='50728')
+#
+# cnx = mysql.connector.connect(user='ece477', password='SET56?ra', \
+#                               host='mysql.stackcp.com',\
+#                               database='cl57-users-xyq', port=50728)
+
+cnx = mysql.connector.connect(user='achhetri', password='aka619ASH', \
+                             host='mydb.ics.purdue.edu ',\
+                             database='achhetri')
 if cnx.is_connected():
-    print('Connected to MySQL database')
+   print('Connected to MySQL database')
 cur = cnx.cursor()
 
-query = ("SELECT * FROM users;")
+query = ("SELECT CONCAT(first_name, ' ', last_name) AS 'Patient Name', "
+   "CASE "
+       "WHEN user_name='ccoluson99' THEN 'ccoluson989' "
+       "ELSE '' "
+   "END AS user_name, "
+   "heart AS 'Heart Rate', "
+   "temp AS 'Body Temperature', "
+   "sweat AS 'Sweat Response' "
+   "FROM users "
+   "INNER JOIN datasets ON users.user_id = datasets.user_id "
+   "WHERE user_name = 'ccoluson99' AND password = '123456';")
 
 cur.execute(query)
 
-for (num, name, email, password, dummy, client, server) in cur:
-  print("{}, {}, {}, {}, {}, {}".format(num, name, email, password, client, server))
+for (full_name, user_name, heart, temp, sweat) in cur:
+ print("{}, {}, {}, {}, {}".format(full_name, user_name, heart, temp, sweat))
 
 #insertquery = ("INSERT INTO users "
 #               "(email, password, nameTag, client, server)"
@@ -22,8 +40,10 @@ newquery = ("SELECT CURDATE();")
 #cur.execute(insertquery)
 cur.execute(newquery)
 for value in cur:
-    print(value[0])
+   print(value[0])
 
 #cnx.commit()
 cur.close()
 cnx.close()
+
+
