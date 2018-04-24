077f111110425433c106a48eb37074eeec5a28d4
Christina Shi <shi207@purdue.edu>
2018-04-23 20:21:16 -0400

    updated stmController to request battery data

--- stmConnectionControl.py
+++ stmConnectionControl.py
@@ -8,16 +8,27 @@ deviceAddress = 0x10
 class stmConnection():
     def __init__(self):
         self.sensorSignal = 0
-    def signalSensorToSTM(sensor):
-        self.sensorSignal = sensor
-        bus.write_byte(deviceAddress, sensor)
+        self.stopRequestingBatteryData = 0
+    def signalSensorToSTM(self,sensor):
+        if sensor is not 4:
+            self.stopRequestingBatteryData = 1
+            self.sensorSignal = sensor
+            bus.write_byte(deviceAddress, sensor)
+        else:
+            if self.stopRequestingBatteryData is 0:
+                self.sensorSignal = sensor
+                bus.write_byte(deviceAddress, sensor)
         return
+    
     def readSensorData():
         if self.sensorSignal is 1:
             top = readDataByteFromI2C(deviceAddress)
             bot = readDataByteFromI2C(deviceAddress)
             return str(top) + '.' + str(bot)
-        else:
+        elif self.sensorSignal is 2 or 3:
             topNum = bin(readDataByteFromI2C(deviceAddress))
             botNum = bin(readDataByteFromI2C(deviceAddress))
             return (int(topNum.split('b')[1] + botNum.split('b')[1],2))
+        else:
+            batteryLevel = readDataByteFromI2C(deviceAddress)
+            return int(batteryLevel)
