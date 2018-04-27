c7efcbbd08aef9566adc7d50e85afb707641d87c
Christina Shi <shi207@purdue.edu>
2018-04-26 20:06:38 -0400

    making a fucking keyboard from fucking scratch

--- stmConnectionControl.py
+++ stmConnectionControl.py
@@ -3,38 +3,22 @@ import sys
 import smbus
 import time
 bus = smbus.SMBus(1)
-deviceAddress = 0x0a
+deviceAddress = 0x10
 
 class stmConnection():
     def __init__(self):
         self.sensorSignal = 0
-        self.stopRequestingBatteryData = 0
-    def signalSensorToSTM(self,sensor):
-        if sensor is not 4:
-            print("Requesting data from sensor #")
-            print(sensor)
-            self.stopRequestingBatteryData = 1
-            self.sensorSignal = sensor
-            bus.write_byte(deviceAddress, sensor)
-        else:
-            if self.stopRequestingBatteryData is 0:
-                self.sensorSignal = sensor
-                bus.write_byte(deviceAddress, sensor)
+    def signalSensorToSTM(sensor):
+        self.sensorSignal = sensor
+        bus.write_byte(deviceAddress, sensor)
         return
-    
-    def readSensorData(self):
+    def readSensorData():
         if self.sensorSignal is 1:
-            print(self.sensorSignal)
-            print("Collecting temperature data")
-            top = bus.read_byte(deviceAddress)
-            bot = bus.read_byte(deviceAddress)
-            return float(str(top) + '.' + str(bot))
-        elif self.sensorSignal is 2 or 3:
-            print("Collecting data from sensor:")
-            print(self.sensorSignal)
-            topNum = bin(bus.read_byte(deviceAddress))
-            botNum = bin(bus.read_byte(deviceAddress))
-            return (int(topNum.split('b')[1] + botNum.split('b')[1],2))
+            top = readDataByteFromI2C(deviceAddress)
+            bot = readDataByteFromI2C(deviceAddress)
+            return str(top) + '.' + str(bot)
         else:
-            batteryLevel = bus.read_byte(deviceAddress)
-            return int(batteryLevel)
+            topNum = bin(readDataByteFromI2C(deviceAddress))
+            botNum = bin(readDataByteFromI2C(deviceAddress))
+            return (int(topNum.split('b')[1] + botNum.split('b')[1],2))
+    
